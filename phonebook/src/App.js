987cb6a890d28eb162ebb9528637a48ec8144cf4
KerogiGenos <joeyaddai0568@gmail.com>
2021-11-02 21:08:40 +0000

    2.7: The Phonebook Step2

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -18,12 +18,17 @@ const App = () => {
       id:1
     }
     setPersons(persons.concat(noteObject))
+    const isPerson =persons.some(p=>p.name===newName)
+
+      if(isPerson){
+      window.alert(`${newName} is already part of the phonebook`)
+    }
     setNewName('')
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Phonebook</h2> 
       <form onSubmit={addPerson} >
         <div>
           name: <input value={newName} onChange={name} />
@@ -34,7 +39,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((names)=>{
-        return <div> {names.name}</div>
+        return <div> <p key={names.name}>{names.name}</p></div>
       })}
     </div>
   )
